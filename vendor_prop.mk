df8b0a17bd2953213857ed6f0dbbd6973cc6ffb4
zqm64 <zqm64@outlook.com>
2020-10-05 15:51:02 +0800

    ulysse-common: Remove all mods

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -239,7 +239,3 @@ ro.cutoff_voltage_mv=3200
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
-
-# Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.device_config.runtime_native.usap_pool_enabled=true
